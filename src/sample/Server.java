cd4dba3c8852b1b419df9bc3086f3d2e7d481e8e
MrTeejay619 <taabish.jeshani@uoit.net>
2018-03-13 13:55:15 -0400

    Updates to Server.java

--- src/sample/Server.java
+++ src/sample/Server.java
@@ -4,5 +4,17 @@ import java.net.*;
 import java.io.*;
 
 public class Server {
-    
+
+    public void runServer(int port) throws IOException{
+
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server is Up, Listening for connections");
+
+        while (true){
+            Socket socket = serverSocket.accept();
+            Thread thread = new Thread(new ClientConnectHandler(socket));
+            thread.start();
+        }
+
+    }
 }
